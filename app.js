fc61c71fdd89452f2421786e9001c5116010147b
KrishnaChaitanyaThota <92036047+KrishnaChaitanyaThota@users.noreply.github.com>
2022-10-09 15:10:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById("login");
+addEventListener("click", () => {
+    window.open("app.html");
+})
+
+document.getElementById("in");
+addEventListener("click", () => {
+    window.open("cam.html");
+})
